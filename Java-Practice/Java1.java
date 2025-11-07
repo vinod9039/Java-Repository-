5ebb1cd31b44ae10d33eea7e7f870e4d4f2b7f67
Vinod  Athya <vinodathya001@gmail.com>
2025-11-07 17:42:18 +0530

    Update Java1.java

--- Java-Practice/Java1.java
+++ Java-Practice/Java1.java
@@ -1,1 +1,31 @@
+public class file1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try
+        {
+          Scanner sc=new Scanner(System.in);
+          String fname,data;
+          System.out.print("Enter file name ");
+          fname=sc.nextLine();
+          fname+=".txt";
+          System.out.print("Enter data ");
+          data=sc.nextLine();
+          FileWriter fw=new FileWriter(fname);
+          fw.write(data);
+          fw.append(data);
+          System.out.println("Your Data save successfully.");
+          fw.close();
+          
+        }
+        catch(IOException e)
+        {
+          System.out.println("Erro"+e);
+          
+        }
+    }
+    
+}
 
